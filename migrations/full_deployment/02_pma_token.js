eac5fced4eb3353755fd1de72af9f7331be1b91a
Ashitosh <ashitosh@deqode.com>
2022-08-22 11:56:53 +0530

    DEC-237: update config

--- migrations/full_deployment/02_pma_token.js
+++ migrations/full_deployment/02_pma_token.js
@@ -1,12 +1,15 @@
 const { saveAddress } = require('../scripts/saveAddress');
-const { PMATokenAddress } = require('../configurations/config');
+const { PMATokenAddress, WBNB_ADDRESS } = require('../configurations/config');
 
 const PMA = artifacts.require('BEP20PMA');
+const WBNB = artifacts.require('WBNB');
+
 const Registry = artifacts.require('Registry');
 
 module.exports = async (deployer) => {
 	try {
 		let PMAContract;
+		let WBNBContract;
 		const networkId = deployer.network_id.toString();
 		const addresses = require(`../configurations/${networkId}/Addresses.json`);
 
@@ -15,13 +18,16 @@ module.exports = async (deployer) => {
 		if (networkId == '1111') {
 			// BEP20 PMA Token
 			PMAContract = await PMA.new();
+			WBNBContract = await WBNB.new();
 		} else {
 			PMAContract = await PMA.at(PMATokenAddress[networkId]);
+			WBNBContract = await WBNB.at(WBNB_ADDRESS[networkId]);
 		}
 
 		const registryContract = await Registry.at(addresses[networkId]['Registry']);
 
 		await registryContract.setAddressFor('PMAToken', PMAContract.address);
+		await registryContract.setAddressFor('WBNBToken', WBNBContract.address);
 
 		await saveAddress('BEP20PMA', PMAContract.address, networkId);
 	} catch (error) {
